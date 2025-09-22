5f65fcf641a1e1a3e4b9e7304f66c61044104f69
ko-morishita-clinks <ko-morishita@clinks.jp>
2025-09-22 11:00:14 +0900

    feat: sec03

--- public/index.php
+++ public/index.php
@@ -1,2 +1,55 @@
 <?php
-echo phpinfo();
+
+$username = $_POST['username'];
+
+
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>2ちゃんねる掲示板</title>
+  <link rel="stylesheet" href="./assets/css/style.css">
+</head>
+<body>
+  <header>
+    <h1 class="title">2ちゃんねる掲示板</h1>
+    <hr>
+  </header>
+
+  <!-- スレッドエリア -->
+   <div class="threadWrapper">
+      <div class="childWrapper">
+        <div class="threadTitle">
+          <span>【タイトル】</span>
+          <h1>2ちゃんねる掲示板を作ってみた</h1>
+        </div>
+        <section>
+          <article>
+            <div class="wrapper">
+              <div class="nameArea">
+                <span>名前：</span>
+                <p class="username">test name</p>
+                <time>：2025/09/22 10:41</time>
+              </div>
+              <p class="comment">手書きのコメントです。</p>
+            </div>
+          </article>
+        </section>
+        <form method="POST" class="formWrapper">
+          <div>
+            <input type="submit" value="書き込む">
+            <label>名前：</label>
+            <input type="text" name="username">
+            <div>
+              <textarea class="commentTextArea" name="" id=""></textarea>
+            </div>
+          </div>
+        </form>
+      </div>
+   </div>
+</body>
+</html>
